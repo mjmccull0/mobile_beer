54b8884a2c9be5a9214c58301ba2a7b7c9df0e53
mjmccull0 <mjmccull@uncg.edu>
2020-11-04 18:26:43 -0500

    Refactored fetching data to remove duplication of code.  Also added api endpoint to useBeer in order to continue with build process, see commit 2717a34.

--- hook/useBeer.js
+++ hook/useBeer.js
@@ -1,99 +1,61 @@
 import { useEffect, useState } from 'react';
-import { useFetch } from './useFetch';
 import Constants from "expo-constants";
 
-const endpoint = Constants.manifest.beerApi.endpoint;
+const endpoint = "https://api.punkapi.com/v2/beers";
 
 export const useBeer = () => {
-  const perPage = 40;
+  const perPage = 25;
   const [page, setPage] = useState(1);
-  const [url, setUrl] = useState(`${endpoint}?_page=${page}&_limit=${perPage}`);
   const [beer, setBeer] = useState([]);
   const [isRefreshing, setIsRefreshing] = useState(false);
-  const [loading, setLoading] = useState(false);
 
-  const { data, error, refresh } = useFetch(url);
 
   useEffect(() => {
-    // There is a better way to handle fetching data and preventing
-    // duplication; but now this will do.
+    fetchBeer().then(data => setBeer(data));
+  }, []);
+
+
+  const search = async (params = {}) => {
+    const results = await fetchBeer(params);
+    setBeer(results);
+  }
+
+  const nextPage = async () => {
     let beerMap = new Map();
-    [...beer, ...data].forEach((beer) => {
+    const results = await fetchBeer({ page: page + 1});
+    [...beer, ...results].forEach((beer) => {
       beerMap.set(beer.id, beer);
     });
+
     setBeer(Array.from(beerMap.values()));
-    setIsRefreshing(false);
-  }, [data])
+    setPage((prev) => prev + 1);
+  }
 
-  const search = (params) => {
-    const abortController = new AbortController();
-    const signal = abortController.signal;
 
-    const newUrl = new URL(url);
+  const handleRefresh = async () => {
+    const results = await fetchBeer({page: 1});
+    setBeer(results);
+    setPage(1);
+  }
+
+  const fetchBeer = async (params = {}) => {
+    const newUrl = new URL(endpoint);
+
     Object.keys(params).map(key => {
       newUrl.searchParams.append(key, params[key]);
     });
 
-    const fetchResults = async () => {
-      try {
-        const response = await fetch(newUrl);
-        const data = await response.json();
-        setBeer(data);
-      } catch (error) {
-        if (!signal.aborted) {
-          setError(error);
-        }
-      }
-    }
-    fetchResults();
-    return () => {
-      abortController.abort();
+    newUrl.searchParams.append('per_page', perPage);
+
+    try {
+      const response = await fetch(newUrl);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
     }
   }
 
-  const nextPage = () => {
-    const newUrl = new URL(url);
-    newUrl.searchParams.append('_page', page + 1);
-
-    const fetchNextPage = async () => {
-      try {
-        const response = await fetch(newUrl);
-        const data = await response.json();
-        let beerMap = new Map();
-        [...beer, ...data].forEach((beer) => {
-          beerMap.set(beer.id, beer);
-        });
-        setBeer(Array.from(beerMap.values()));
-      } catch (error) {
-        if (!signal.aborted) {
-          setError(error);
-        }
-      } finally {
-        setPage((prev) => prev + 1);
-      }
-    }
-    fetchNextPage();
-  }
-
-  const handleRefresh = () => {
-    const newUrl = new URL(url);
-    newUrl.searchParams.append('_page', 1);
-
-    const refresh = async () => {
-      try {
-        const response = await fetch(newUrl);
-        const data = await response.json();
-        setBeer(data);
-      } catch (error) {
-        if (!signal.aborted) {
-          setError(error);
-        }
-      } finally {
-        setPage(1);
-      }
-    }
-    refresh();
-  }
 
   return [ beer, nextPage, handleRefresh, isRefreshing, search ];
 }
